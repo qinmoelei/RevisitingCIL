f09a633408e0bed6904a5cf2581a3e1f63c75bd6
Qin Molei <937883273@qq.com>
2023-10-11 18:21:37 +0800

    Revert "update branch"
    This reverts commit b45749292d8f7a0fd6f3df45423eeaf3d2567620.

--- models/adam_ensemble.py
+++ models/adam_ensemble.py
@@ -7,71 +7,40 @@ from tqdm import tqdm
 from torch import optim
 from torch.nn import functional as F
 from torch.utils.data import DataLoader
-from utils.inc_net import (
-    IncrementalNet,
-    SimpleCosineIncrementalNet,
-    MultiBranchwithfcCosineIncrementalNet,
-    SimpleVitNet_linear,
-    SimpleVitNet,
-)
+from utils.inc_net import IncrementalNet,SimpleCosineIncrementalNet,MultiBranchwithfcCosineIncrementalNet,SimpleVitNet_linear
 from models.base import BaseLearner
 from utils.toolkit import target2onehot, tensor2numpy
 from types import SimpleNamespace
-from convs.vision_transformer_adapter_LoRA import (
-    vit_base_patch16_224_in21k_adapter_lora,
-)
-from convs.vpt import build_promptmodel
-import timm
-
+from convs.vision_transformer_adapter_LoRA import vit_base_patch16_224_in21k_adapter_lora
 # tune the model at first session with adapter, and then conduct simplecil.
 num_workers = 8
-
-
 # 这个concat出来两个结果，一个是simplevit出来的最正常的adapater，一个是目前调出来的linear+fc的结果
 # 现在调整一个adapater的参数 看效果如何
 class Learner(BaseLearner):
     def __init__(self, args):
         super().__init__(args)
-        self.batch_size = args["batch_size"]
-        self.init_lr = args["init_lr"]
-        self.weight_decay = (
-            args["weight_decay"] if args["weight_decay"] is not None else 0.0005
-        )
-        self.min_lr = args["min_lr"] if args["min_lr"] is not None else 1e-8
-        self.args = args
-        self.args_args = SimpleNamespace(**self.args)
+        self. batch_size= args["batch_size"]
+        self. init_lr=args["init_lr"]
+        self.weight_decay=args["weight_decay"] if args["weight_decay"] is not None else 0.0005
+        self.min_lr=args['min_lr'] if args['min_lr'] is not None else 1e-8
+        self.args=args
+        self.args_args=SimpleNamespace(**self.args)
         _network = SimpleVitNet_linear(args, True)
-        _network.convnet = vit_base_patch16_224_in21k_adapter_lora(
-            tuning_config=self.args_args
-        )
+        _network.convnet=vit_base_patch16_224_in21k_adapter_lora(tuning_config=self.args_args)
         _network.convnet.out_dim = 768
-        self._network_list = []
-        self._network_list.append(_network)
-
-        _network = SimpleVitNet(args, True)
-        _network.convnet = timm.create_model(
-            "vit_base_patch16_224_in21k_ssf", pretrained=True, num_classes=0
-        )
-        _network.convnet.out_dim = 768
+        self._network_list=[]
         self._network_list.append(_network)
         
-        _network = SimpleVitNet(args, True)
-        _network.convnet = build_promptmodel(
-                modelname='vit_base_patch16_224_in21k',
-                Prompt_Token_num=args.get("prompt_token_num",10),
-                VPT_type=args.get("vpt_type","Shallow"),
-            )
-        prompt_state_dict = _network.convnet.obtain_prompt()
-        _network.convnet.load_prompt(prompt_state_dict)
+        _network = SimpleVitNet_linear(args, True)
+        _network.convnet=vit_base_patch16_224_in21k_adapter_lora(tuning_config=self.args_args)
         _network.convnet.out_dim = 768
-        self._network_list.append(_network)
         
         
 
     def after_task(self):
         self._known_classes = self._total_classes
-
-    def replace_fc(self, trainloader, model, args):
+    
+    def replace_fc(self,trainloader, model, args):
         # replace fc.weight with the embedding average of train data
         model = model.eval()
         embedding_list = []
@@ -79,122 +48,85 @@ class Learner(BaseLearner):
         # data_list=[]
         with torch.no_grad():
             for i, batch in enumerate(trainloader):
-                (_, data, label) = batch
-                data = data.cuda()
-                label = label.cuda()
-                embedding = model(data)["features"]
+                (_,data,label)=batch
+                data=data.cuda()
+                label=label.cuda()
+                embedding = model(data)['features']
                 embedding_list.append(embedding.cpu())
                 label_list.append(label.cpu())
         embedding_list = torch.cat(embedding_list, dim=0)
         label_list = torch.cat(label_list, dim=0)
 
-        class_list = np.unique(self.train_dataset.labels)
+        class_list=np.unique(self.train_dataset.labels)
         proto_list = []
         for class_index in class_list:
             # print('Replacing...',class_index)
-            data_index = (label_list == class_index).nonzero().squeeze(-1)
-            embedding = embedding_list[data_index]
-            proto = embedding.mean(0)
-            self._network.fc.weight.data[class_index] = proto
+            data_index=(label_list==class_index).nonzero().squeeze(-1)
+            embedding=embedding_list[data_index]
+            proto=embedding.mean(0)
+            self._network.fc.weight.data[class_index]=proto
         return model
+
+    
 
     def incremental_train(self, data_manager):
         self._cur_task += 1
-        self._total_classes = self._known_classes + data_manager.get_task_size(
-            self._cur_task
-        )
+        self._total_classes = self._known_classes + data_manager.get_task_size(self._cur_task)
         self._network.update_fc(self._total_classes)
-        logging.info(
-            "Learning on {}-{}".format(self._known_classes, self._total_classes)
-        )
+        logging.info("Learning on {}-{}".format(self._known_classes, self._total_classes))
 
-        train_dataset = data_manager.get_dataset(
-            np.arange(self._known_classes, self._total_classes),
-            source="train",
-            mode="train",
-        )
-        self.train_dataset = train_dataset
-        self.data_manager = data_manager
-        self.train_loader = DataLoader(
-            train_dataset,
-            batch_size=self.batch_size,
-            shuffle=True,
-            num_workers=num_workers,
-        )
+        train_dataset = data_manager.get_dataset(np.arange(self._known_classes, self._total_classes),source="train", mode="train", )
+        self.train_dataset=train_dataset
+        self.data_manager=data_manager
+        self.train_loader = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=num_workers)
 
-        test_dataset = data_manager.get_dataset(
-            np.arange(0, self._total_classes), source="test", mode="test"
-        )
-        self.test_loader = DataLoader(
-            test_dataset,
-            batch_size=self.batch_size,
-            shuffle=False,
-            num_workers=num_workers,
-        )
+        test_dataset = data_manager.get_dataset(np.arange(0, self._total_classes), source="test", mode="test" )
+        self.test_loader = DataLoader(test_dataset, batch_size=self.batch_size, shuffle=False, num_workers=num_workers)
 
-        train_dataset_for_protonet = data_manager.get_dataset(
-            np.arange(self._known_classes, self._total_classes),
-            source="train",
-            mode="test",
-        )
-        self.train_loader_for_protonet = DataLoader(
-            train_dataset_for_protonet,
-            batch_size=self.batch_size,
-            shuffle=True,
-            num_workers=num_workers,
-        )
+        train_dataset_for_protonet=data_manager.get_dataset(np.arange(self._known_classes, self._total_classes),source="train", mode="test", )
+        self.train_loader_for_protonet = DataLoader(train_dataset_for_protonet, batch_size=self.batch_size, shuffle=True, num_workers=num_workers)
 
         if len(self._multiple_gpus) > 1:
-            print("Multiple GPUs")
+            print('Multiple GPUs')
             self._network = nn.DataParallel(self._network, self._multiple_gpus)
         self._train(self.train_loader, self.test_loader, self.train_loader_for_protonet)
         if len(self._multiple_gpus) > 1:
             self._network = self._network.module
 
     def _train(self, train_loader, test_loader, train_loader_for_protonet):
+        
         self._network.to(self._device)
-
+        
         if self._cur_task == 0:
             # show total parameters and trainable parameters
             total_params = sum(p.numel() for p in self._network.parameters())
-            print(f"{total_params:,} total parameters.")
+            print(f'{total_params:,} total parameters.')
             total_trainable_params = sum(
-                p.numel() for p in self._network.parameters() if p.requires_grad
-            )
-            print(f"{total_trainable_params:,} training parameters.")
+                p.numel() for p in self._network.parameters() if p.requires_grad)
+            print(f'{total_trainable_params:,} training parameters.')
             if total_params != total_trainable_params:
                 for name, param in self._network.named_parameters():
                     if param.requires_grad:
                         print(name, param.numel())
-            if self.args["optimizer"] == "sgd":
-                optimizer = optim.SGD(
-                    self._network.parameters(),
-                    momentum=0.9,
-                    lr=self.init_lr,
-                    weight_decay=self.weight_decay,
-                )
-            elif self.args["optimizer"] == "adam":
-                optimizer = optim.AdamW(
-                    self._network.parameters(),
-                    lr=self.init_lr,
-                    weight_decay=self.weight_decay,
-                )
-            scheduler = optim.lr_scheduler.CosineAnnealingLR(
-                optimizer, T_max=self.args["tuned_epoch"], eta_min=self.min_lr
-            )
+            if self.args['optimizer']=='sgd':
+                optimizer = optim.SGD(self._network.parameters(), momentum=0.9, lr=self.init_lr,weight_decay=self.weight_decay)
+            elif self.args['optimizer']=='adam':
+                optimizer=optim.AdamW(self._network.parameters(), lr=self.init_lr, weight_decay=self.weight_decay)
+            scheduler=optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=self.args['tuned_epoch'], eta_min=self.min_lr)
             self._init_train(train_loader, test_loader, optimizer, scheduler)
             self.construct_dual_branch_network()
         else:
             pass
         self.replace_fc(train_loader_for_protonet, self._network, None)
+            
 
     def construct_dual_branch_network(self):
         network = MultiBranchwithfcCosineIncrementalNet(self.args, True)
         network.construct_dual_branch_network(self._network)
-        self._network = network.to(self._device)
+        self._network=network.to(self._device)
 
     def _init_train(self, train_loader, test_loader, optimizer, scheduler):
-        prog_bar = tqdm(range(self.args["tuned_epoch"]))
+        prog_bar = tqdm(range(self.args['tuned_epoch']))
         for _, epoch in enumerate(prog_bar):
             self._network.train()
             losses = 0.0
@@ -213,6 +145,8 @@ class Learner(BaseLearner):
                 _, preds = torch.max(logits, dim=1)
                 correct += preds.eq(targets.expand_as(preds)).cpu().sum()
                 total += len(targets)
+            
+                
 
             scheduler.step()
             train_acc = np.around(tensor2numpy(correct) * 100 / total, decimals=2)
@@ -221,7 +155,7 @@ class Learner(BaseLearner):
             info = "Task {}, Epoch {}/{} => Loss {:.3f}, Train_accy {:.2f}, Test_accy {:.2f}".format(
                 self._cur_task,
                 epoch + 1,
-                self.args["tuned_epoch"],
+                self.args['tuned_epoch'],
                 losses / len(train_loader),
                 train_acc,
                 test_acc,
@@ -229,3 +163,7 @@ class Learner(BaseLearner):
             prog_bar.set_description(info)
 
         logging.info(info)
+
+    
+
+
